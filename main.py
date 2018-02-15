a8645e8082c308b0d1584fc5f564acef9ff10a9c
JamesSmith1202 <jsmith@stuy.edu>
2018-02-15 15:34:23 -0500

    work on adding db to server

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+#------------PROBLEMS------------#
+#The program is only reading in one entry from the json
+#------------PROBLEMS------------#
+
+
 #http://www.vizgr.org/historical-events/search.php?format=json&begin_date=-3000000&end_date=20151231&lang=en
 
 from pymongo import MongoClient
-import pprint
+from json import load
+
 
 connection = MongoClient('homer.stuy.edu')
-db = collection.stan_smith_xus #the database
-#populate here
-connection.add_one(events)
+#if the database hasnt been made, then make it and add it
+if not 'stan_smith_xus' in connection.database_names():    
+    print "i tried"
+    collection = connection.stan_smith_xus #the database
+    db = collection.events
+    with open('events.json') as f:
+        event = load(f)
+        print event
+        db.insert_one(event)
+else: #for debugging, the db is removed if you run the program after making a db on the server
+    print "removing..."
+    connection.drop_database('stan_smith_xus')
