18a3dddd5f905d46e764139670c553a39df658cc
Jacqueline Xu <jackiestuy@gmail.com>
2018-02-25 19:43:03 -0500

    minor change

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ from json import load
 
 connection = MongoClient('homer.stuy.edu')
 #if the database hasnt been made, then make it and add it
-if not 'stan_smith_xus' in connection.database_names():    
+if not 'stan_smith_xus' in connection.database_names():
     print "i tried"
     collection = connection.stan_smith_xus #the database
     db = collection.events
     with open('events.json') as f:
         event = load(f)
         print event
-        db.insert_one(event)
+        db.insert(event)
 else: #for debugging, the db is removed if you run the program after making a db on the server
     print "removing..."
     connection.drop_database('stan_smith_xus')
